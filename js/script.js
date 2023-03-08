d5f50244f32107a87436ad003f4a0fc1485f8555
Suyeon Lee <slee1055@my.bcit.ca>
2023-03-08 15:43:43 -0800

    define a courseList and format each item to a JS object.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const courseList = ["course1", "course2", "course3"]
+
+let course1 = {
+    code: "ACIT 1420",
+    name: "Introduction to Systems Administration"
+}
+
+let course2 = {
+    code: "ACIT 1620",
+    name: "Fundamental Web Technologies"
+}
+
+let course3 = {
+    code: "ACIT 1630",
+    name: "Database System"
+}
