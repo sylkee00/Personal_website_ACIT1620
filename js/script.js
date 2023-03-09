9ce88ee24be128348d14653b92b00f98100550fa
Suyeon Lee <slee1055@my.bcit.ca>
2023-03-08 16:38:35 -0800

    use prompt to get 4-digit number of course from user and write a loop to check if the code property contains the input and also the input is invalid. Lastly, log to console the output(if not add a new object to the courseList).

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-const courseList = ["course1", "course2", "course3"]
+const courseList = [
+    {
+      code: "ACIT 1420",
+      name: "Introduction to Systems Administration"
+    },
+    {
+        code: "ACIT 1620",
+        name: "Fundamental Web Technologies"
+    },
+    {
+        code: "ACIT 1630",
+        name: "Database System"
+    }
+  ];
 
-let course1 = {
-    code: "ACIT 1420",
-    name: "Introduction to Systems Administration"
+let user = prompt("Please enter the 4-digit number of course code: ");
+while (isNaN(user) || user.length !== 4 ) {
+    user = prompt("Invalid input, Please enter the 4-digit number of course code: ");
 }
 
-let course2 = {
-    code: "ACIT 1620",
-    name: "Fundamental Web Technologies"
+let courseFound = false;
+
+for(let course of courseList){
+    if(course["code"].includes(user)){
+        console.log(`Yes I am taking the course: ${course["code"]} - ${course["name"]}`)
+        courseFound = true;
+        break;
+    } 
 }
 
-let course3 = {
-    code: "ACIT 1630",
-    name: "Database System"
+if (!courseFound) {
+  const newCourse = {code: user, name: null };
+  courseList.push(newCourse);
+  console.log("Updated course list:");
+  console.log(courseList);
 }
