f2608b7d002472abe7056628b4ab1447cd272035
Suyeon Lee <slee1055@my.bcit.ca>
2023-04-19 01:51:14 -0700

    Make better design modifying color and font

--- js/script.js
+++ js/script.js
@@ -1,41 +1,3 @@
-// const courseList = [
-//     {
-//       code: "ACIT 1420",
-//       name: "Introduction to Systems Administration"
-//     },
-//     {
-//         code: "ACIT 1620",
-//         name: "Fundamental Web Technologies"
-//     },
-//     {
-//         code: "ACIT 1630",
-//         name: "Database System"
-//     }
-//   ];
-
-// let user = prompt("Please enter the 4-digit number of course code: ");
-// while (isNaN(user) || user.length !== 4 ) {
-//     user = prompt("Invalid input, Please enter the 4-digit number of course code: ");
-// }
-
-// let courseFound = false;
-
-// for(let course of courseList){
-//     if(course["code"].includes(user)){
-//         console.log(`Yes I am taking the course: ${course["code"]} - ${course["name"]}`)
-//         courseFound = true;
-//         break;
-//     } 
-// }
-
-// if (!courseFound) {
-//   const newCourse = {code: user, name: null };
-//   courseList.push(newCourse);
-//   console.log("Updated course list:");
-//   console.log(courseList);
-// }
-
-
 function createCourseArray() {
   let courseArray = [];
   let courseListItems = document.querySelectorAll('.courseList li');
@@ -85,7 +47,7 @@ function findCourse(courseList) {
       }
       console.log(courseCode)
       if(courseCode.includes(user)) {
-          courseListItem.style.backgroundColor = 'green';
+          courseListItem.style.backgroundColor = '#f2f2f2';
           courseFound = true;
       }
   }
